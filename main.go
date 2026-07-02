f2eac9691f7a0cda183fd9282751455cde754761
sandu <sandu.grecu88@gmail.com>
2026-07-02 15:50:06 +0300

    Add error handling

--- main.go
+++ main.go
@@ -32,16 +32,30 @@ func main() {
 		switch chosenOption {
 		case "1":
 			tasks = addTask(tasks, reader)
+
 		case "2":
 			listTasks(tasks)
+
 		case "3":
 			fmt.Print("What is the index of the task you want to change the status: ")
-			index := getIndex(reader)
+			index, err := getIndex(reader)
+			if err != nil {
+				fmt.Println("Invalid index. Index must be an integer number.")
+				continue
+			}
 			changeStatus(&tasks, index)
+
 		case "4":
 			fmt.Print("What is the index of the task you want to delete: ")
-			index := getIndex(reader)
+			index, err := getIndex(reader)
+			if err != nil {
+				fmt.Println("Invalid index. Index must be an integer number.")
+				continue
+			}
 			deleteTask(&tasks, index)
+
+		default:
+			fmt.Println("Invalid option! Choose a number in range 1-4.")
 		}
 	}
 }
@@ -58,6 +72,11 @@ func addTask(tasks []Task, reader *bufio.Reader) []Task {
 	fmt.Println("What is the task:")
 	task, _ := reader.ReadString('\n')
 	task = strings.TrimSpace(task)
+	if task == "" {
+		// If task is empty the original tasks slice will be returned
+		return tasks
+	}
+
 	return append(tasks, NewTask(task))
 }
 
@@ -75,19 +94,29 @@ func listTasks(tasks []Task) {
 }
 
 func changeStatus(tasks *[]Task, index int) {
+	if index < 0 || index >= len(*tasks) {
+		fmt.Println("Task index does not exist.")
+		return
+	}
+
 	(*tasks)[index].isDone = !(*tasks)[index].isDone
 }
 
 func deleteTask(tasks *[]Task, index int) {
+	if index < 0 || index >= len(*tasks) {
+		fmt.Println("Task index does not exist.")
+		return
+	}
+
 	*tasks = append((*tasks)[:index], (*tasks)[index+1:]...)
 }
 
-func getIndex(reader *bufio.Reader) int {
+func getIndex(reader *bufio.Reader) (int, error) {
 	text, _ := reader.ReadString('\n')
 	indexStr := strings.TrimSpace(text)
 	index, err := strconv.Atoi(indexStr)
 	if err != nil {
-		fmt.Println("Invalid index")
+		return 0, err
 	}
-	return index
+	return index, nil
 }
